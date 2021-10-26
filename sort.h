cbe8871028f2f70886d120780a3a37e263895d90
samson tedla <samitedla23@gmail.com>
2021-10-26 22:26:11 +0000

    quick sort algorithim and big O notation

--- sort.h
+++ sort.h
@@ -19,4 +19,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int partition_array(int *array, size_t size, int head, int tail);
+void lomuto_sort(int *array, size_t size, int head, int tail);
+void quick_sort(int *array, size_t size);
 #endif /* SORT_H */
